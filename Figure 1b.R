4242bf54441a293c8ede61c8cf2e4a813625dfd8
KatNicholls <113170744+KatNicholls@users.noreply.github.com>
2022-12-14 10:26:22 +0000

    Create Figure 1b.R

---
+++ Figure 1b.R
@@ -0,0 +1,13 @@
+# Figure for Spread of Bacteria and vector Globally
+library(dplyr)
+library(rworldmap)
+library(countrycode)
+library(RColorBrewer)
+
+setwd('~/II/CRIT REVIEW ESSAY/FIGURE DATA')
+
+#Data
+DATA <- read.csv('BACTERIA SPREAD.csv')
+
+
+
